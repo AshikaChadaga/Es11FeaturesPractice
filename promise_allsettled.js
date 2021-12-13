ea1560b4d6091f8c820827fc1e074b71f105ffcc
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-13 14:10:20 +0530

    [Ashika]ADDED: Demonstration of promise allsettled

---
+++ promise_allsettled.js
@@ -0,0 +1,17 @@
+promise_1 = Promise.resolve('hi')
+promise_2 = new Promise((resolve, reject) => setTimeout(reject, 100, 'world'))
+
+Promise.all([promise_1, promise_2])
+    .then(([promise_1_result, promise_2_result]) => {
+        console.log(promise_1_result) // output is: {status: 'fulfilled', value: 'hi'}
+        console.log(promise_2_result) // output is: {status: 'rejected', reason: 'world'}
+    })
+    .catch((error) => {
+        console.log('error : ' + error);
+    });
+
+Promise.allSettled([promise_1, promise_2])
+    .then(([promise_1_result, promise_2_result]) => {
+        console.log(promise_1_result) // output is: {status: 'fulfilled', value: 'hi'}
+        console.log(promise_2_result) // output is: {status: 'rejected', reason: 'world'}
+    })
