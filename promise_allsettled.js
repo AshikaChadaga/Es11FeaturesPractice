cf1e1b389d5617852acdbe257e16e1d6e6b26a7f
AshikaChadaga <ashikachadaga@gmail.com>
2021-12-14 08:17:46 +0530

    [Ashika]REFACTORED: Removed comment

--- promise_allsettled.js
+++ promise_allsettled.js
@@ -3,8 +3,8 @@ promise_2 = new Promise((resolve, reject) => setTimeout(reject, 100, 'world'))
 
 Promise.all([promise_1, promise_2])
     .then(([promise_1_result, promise_2_result]) => {
-        console.log(promise_1_result) // output is: {status: 'fulfilled', value: 'hi'}
-        console.log(promise_2_result) // output is: {status: 'rejected', reason: 'world'}
+        console.log(promise_1_result) 
+        console.log(promise_2_result) 
     })
     .catch((error) => {
         console.log('error : ' + error);
